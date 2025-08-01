f309e50d1e33ef40aac41941666628c0d3044547
VihanGamage <vihanchandula@gmail.com>
2025-08-01 18:02:43 +0530

    defaultconnection added

--- backend/Models/Product.cs
+++ backend/Models/Product.cs
@@ -6,3 +6,5 @@ public class Product
     public string name {get; set;}
     public decimal price {get; set;}
 }
+
+//"DefaultConnection": "server=localhost;port=3306;database=aspNet;user=root;password=vihanviha;",
